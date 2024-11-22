a9f9c7bf54b813c52356a42ce4b42a20767538f0
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2024-11-22 13:02:54 +0200

    created API's for Genre GET api/cinema/<entity>/ - should return a list of the all entity items POST api/cinema/<entity>/ - should create a new entity based on passed data GET api/cinema/<entity>/<pk>/ - should return an entity with given id PUT api/cinema/<entity>/<pk>/ - should update the entity with given id based on passed data

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from cinema.models import Movie
+from cinema.models import Movie, Genre
 
 
 class MovieSerializer(serializers.Serializer):
@@ -8,6 +8,7 @@ class MovieSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     description = serializers.CharField()
     duration = serializers.IntegerField()
+
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
@@ -22,3 +23,39 @@ class MovieSerializer(serializers.Serializer):
         instance.save()
 
         return instance
+
+
+
+class ActorSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    first_name = serializers.CharField(max_length=255)
+    last_name = serializers.CharField(max_length=255)
+
+
+# class GenreSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(max_length=255)
+#
+#     def create(self, validated_data):
+#         return Genre.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get("name", instance.name)
+#         instance.save()
+#         return instance
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
+class CinemaHallSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+    rows = serializers.IntegerField()
+    seats_in_row = serializers.IntegerField()
+
+
+
